ceef4840f8c647f0405ee478ac495e7adcab3017
buckket <buckket@cock.li>
2016-12-21 20:52:02 +0100

    thnx checkbashisms

--- altlast.sh
+++ altlast.sh
@@ -19,36 +19,36 @@
 # For this script to work you need a running copy of:
 # https://github.com/sferik/t
 
+# Options:
 favs=1000 # Number of favs to consider
 
 # This script will create various files:
-# followings.txt -> users you currently follow
-# favorites.txt -> users you have faved in the given period
-# legacy.txt -> users you follow but haven’t faved in a while
-# unfollowed.txt -> users you have unfollowed using altlast.sh
+# followings -> users you currently follow
+# favorites -> users you have faved in the given period
+# legacy -> users you follow but haven’t faved in a while
+# unfollowed -> users you have unfollowed using altlast.sh
 
-# Protip: Use "comm -12 followings.txt favorites.txt" to see 
+# Protip: Use "comm -12 followings favorites" to see 
 # which users you’ve faved but currently don’t follow
 
-t followings -l | tail -n +2 | awk '{print $13'} | sort -u > followings.txt
-t favorites -l -n ${favs} | tail -n +2 | awk '{print $5'} | sort -u > favorites.txt
+t followings -l | tail -n +2 | awk '{print $13'} | sort -u > followings
+t favorites -l -n ${favs} | tail -n +2 | awk '{print $5'} | sort -u > favorites
 
-legacy=$(comm -23 followings.txt favorites.txt)
-echo "${legacy}" > legacy.txt
+comm -23 followings favorites > legacy
 
-echo "[-] Found $(wc -l <<< ${legacy}) accounts which you haven’t faved in a while"
+printf "[-] Found %d accounts which you haven’t faved in a while\n" "$(wc -l < legacy)"
 
-for user in ${legacy}; do
-    echo -e "\n[-] Showing details for user ${user}:"
+for user in $(cat legacy); do
+    printf "\n[-] Showing details for user %s\n" "${user}:"
     t whois ${user}
-    echo -n "[?] Do you want to unfollow ${user}? [yN]: "
+    printf "[?] Do you want to unfollow %s? [yN]: " "${user}"
     
     read answer
     case ${answer} in
         "y")
             t unfollow ${user} > /dev/null
-            echo "[x] You’ve unfollowed ${user}"
-            echo "${user}" >> unfollowed.txt
+            printf "[x] You’ve unfollowed %s\n" "${user}"
+            echo "${user}" >> unfollowed
             ;;
     esac
 done
